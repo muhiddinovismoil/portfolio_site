bfb09a1d7bbdb7940af5e9647e44ce18b3364799
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-05-10 16:39:58 +0500

    initial commit my portfolio site

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,19 @@
+export function Footer() {
+    return (
+        <>
+            <footer className="pb-[160px]">
+                <div className="container">
+                    <div className="flex flex-col justify-between space-y-[18px]">
+                        <div className="max-w-[1552px] rounded-[100px] bg-[#131313]">
+                            <h3 className="pl-[70px] pt-[30px] pb-[25px] text-[#6e6e6e]">
+                                Follow me
+                            </h3>
+                            <div></div>
+                        </div>
+                        <p>Muhiddinov's © 2024</p>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <>
+            <div className="max-w-[622px] bg-"></div>
+        </>
+    );
+}
